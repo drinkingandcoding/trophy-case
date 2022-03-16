534384aac421c6b60bd3619b1c9a5e778e82fa85
Jason Stephens <jassteph@redhat.com>
2022-03-16 16:30:43 -0400

    building from cruip's open-react-template

---
+++ src/components/sections/GenericSection.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import SectionHeader from './partials/SectionHeader';
+import { SectionProps } from '../../utils/SectionProps';
+
+const propTypes = {
+  children: PropTypes.node,
+  ...SectionProps.types
+}
+
+const defaultProps = {
+  children: null,
+  ...SectionProps.defaults
+}
+
+const GenericSection = ({
+  className,
+  children,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const sectionHeader = {
+    title: 'Game Title',
+    paragraph: 'Achievements for <Game Title>'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+GenericSection.propTypes = propTypes;
+GenericSection.defaultProps = defaultProps;
+
+export default GenericSection;
---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+import GenericSection from '../components/sections/GenericSection';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      <GenericSection />
+    </>
+  );
+}
+
+export default Home;
